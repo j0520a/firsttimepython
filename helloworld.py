3a05b80ba8145ecfa30d9b764ad547006c0e126b
j0520a <jhall0520@hotmail.com>
2025-01-02 20:24:06 -0700

    first commit

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+def greet():
+    print("My name is Josh")
+
+def add_numbers(a, b):
+    print("The result is ")
+    print(a + b)
+
+def multiply_numbers(a, b):
+    print("The result is ")
+    print(a * b)
+
+def print_phrases(phrase_one, phrase_two):
+    print(phrase_one + " " + phrase_two)
+
+greet()
+print("Hello World")
+print("I'm learning Python ")
+add_numbers(2,2)
+multiply_numbers(4,4)
+print_phrases("You are", "a dummy")
