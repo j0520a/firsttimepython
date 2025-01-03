dccf52eb65f2a1b8cfb19289f178e35839e14732
j0520a <jhall0520@hotmail.com>
2025-01-02 21:22:08 -0700

    third commit

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,9 @@
+#indent your if statements and functions!!!
+#so python knows when to check before running
+#the code. Otherwise, it will run if not indented.
+#must be indented twice if nested
+
+
 def greet():
     print("My name is Josh")
 
@@ -32,3 +38,65 @@ farewell("Charlie")
 
 # don't modify below this line
 farewell("NeetCode")
+
+def two_sum(a, b):
+    print(a + b)
+
+def three_sum(a, b, c):
+    print(a + b + c)
+
+two_sum(7, 10)
+three_sum(3,5,8)
+
+
+# do not modify below this line
+two_sum(10, 9)
+three_sum(5, 14, 6)
+
+def product(a, b):
+    return a * b
+
+
+
+# don't modify below this line
+print(product(2, 4))
+print(product(8, 2))
+print(product(4, 8))
+print(product(8, 8))
+
+def check_equal(x, y) -> bool:
+    return x == y
+
+def check_not_equal(x, y) -> bool:
+    return x != y
+
+def check_less_than(x, y) -> bool:
+    return x < y
+
+def check_greater_than(x, y) -> bool:
+    return x > y
+
+def check_less_than_or_equal(x, y) -> bool:
+    return x <= y
+
+def check_greater_than_or_equal(x, y) -> bool:
+    return x >= y
+
+# Don't change below this line
+print("2 is equal to 2:", check_equal(2, 2))
+print("-2 is equal to 2:", check_equal(-2, 2))
+
+print("-2 is not equal to 2:", check_not_equal(-2, 2))
+print("2 is not equal to 2:", check_not_equal(2, 2))
+
+print("2 is less than 3:", check_less_than(2, 3))
+print("3 is less than 3:", check_less_than(3, 3))
+
+print("3 is greater than 2:", check_greater_than(3, 2))
+print("3 is greater than 2:", check_greater_than(3, 3))
+
+print("3 is less than or equal to 3:", check_less_than_or_equal(3, 3))
+print("4 is less than or equal to 3:", check_less_than_or_equal(4, 3))
+
+print("3 is greater than or equal to 3:", check_greater_than_or_equal(3, 3))
+print("2 is greater than or equal to 3:", check_greater_than_or_equal(2, 3))
